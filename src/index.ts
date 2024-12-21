f432e5918c7d1fbde0d9e5e5b3b0c7950c2ba2e7
forwhosesakes <hayaw@Hayas-MacBook-Air.local>
2024-12-21 22:27:38 +0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ app.get('/', (c) => {
 app.use(
 	"/api/auth/**", // or replace with "*" to enable cors for all routes
 	cors({
-		origin: "*", // replace with your origin
+		origin: ["http://localhost:5173","https://dev.dashboard-fe-aa2.pages.dev"], // replace with your origin
 		allowHeaders: ["Content-Type", "Authorization"],
 		allowMethods: ["POST", "GET", "OPTIONS"],
 		exposeHeaders: ["Content-Length"],
